cbf2d07aa49f65a6212d9c6a1a9f5f234df1e4ac
anaribeiro142 <a_m_s_ribeiro@hotmail.com>
2019-04-06 20:51:54 +0100

    a

--- generator/Patch.cpp
+++ generator/Patch.cpp
@@ -14,3 +14,46 @@ void Patch::multMatrixVector(float *m, float *v, float *res){
         res[j] += v[k] * m[j * 4 + k];
     }
 }
+
+vector<Point> getPatchPoints(int patch){
+    vector<Point> points;
+    vector<int> indexesControlPoints = patchs.at(patch);
+
+    float coordenadasX[4][4], coordenadasY[4][4], coordenadasZ[4][4];
+    float u,v,uu,vv;
+    float t = 1.0f /tessellation;
+    int pos = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Point controlPoint = controlPoints[indexesControlPoints[pos]];
+            coordenadasX[i][j] = controlPoint.getX();
+            coordenadasY[i][j] = controlPoint.getY();
+            coordenadasZ[i][j] = controlPoint.getZ();
+            pos++;
+        }
+    }
+
+
+    for(int i = 0; i < tessellation; i++)
+    {
+        for (int j = 0; j < tessellation; j++)
+        {
+            u = i*t;
+            v = j*t;
+            uu = (i+1)*t;
+            vv = (j+1)*t;
+            Point *p0,*p1,*p2,*p3;
+            p0 = getPoint(u, v, coordenadasX, coordenadasY, coordenadasZ);
+	    p1 = getPoint(u, vv, coordenadasX, coordenadasY, coordenadasZ);
+            p2 = getPoint(uu, v, coordenadasX, coordenadasY, coordenadasZ);
+            p3 = getPoint(uu, vv, coordenadasX, coordenadasY, coordenadasZ);
+
+            points.push_back(*p0); points.push_back(*p2); points.push_back(*p1);
+            points.push_back(*p1); points.push_back(*p2); points.push_back(*p3);
+        }
+    }
+    return points;
+}
--- generator/headers/Patch.h
+++ generator/headers/Patch.h
@@ -2,19 +2,21 @@
 #define __PATCH_H__
 
 #include <vector>
-
+#include <map>
 #include "Point.h"
 
 using namespace std;
 
 class Patch{
-
+	int tessellation;
 	vector<Point*> controlPoints;
+	map<int,vector<int> > patchs;
 
 public:
 	Patch();
 	Patch(vector<Point*> p);
 	void multMatrixVector(float *m, float *v, float *res);
+	vector<Point> getPatchPoints(int patch);
 };
 
 #endif
